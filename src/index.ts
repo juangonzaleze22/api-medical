8dba2f5b3c6607b16057e45a1a51bc3a18a7fdc0
Juan Gonzalez <juangonzaleze04@gmail.com>
2024-06-11 01:24:21 -0400

    first push configuration, register and login basic

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import { router } from "./routes";
+import db from "./config/mongo";
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+    res.json({
+        text: 'welcome'
+    });
+});
+app.use(router);
+
+db().then(() => console.log("Connection to BD ready"));
+
+app.listen(PORT, () => console.log(`Listen port: ${PORT}`));
